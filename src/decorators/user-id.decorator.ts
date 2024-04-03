215250f4f58ccd44ea4752aad70e107e3fef13c5
emsfsilva <ems.fsilva.pe@gmail.com>
2024-04-02 21:10:23 -0300

    feat:create decorator userId

---
+++ src/decorators/user-id.decorator.ts
@@ -0,0 +1,9 @@
+import { authorizationToLoginPayload } from 'src/utils/base-64-converter';
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
+  const { authorization } = ctx.switchToHttp().getRequest().headers;
+  const loginPayload = authorizationToLoginPayload(authorization);
+  //console.log('authorization', authorization);
+  return loginPayload?.id;
+});
