e4621cda812e167ea9e67beb9c35201b8ad37c3f
emsfsilva <ems.fsilva.pe@gmail.com>
2024-05-14 19:14:04 -0300

    feat:create Payment service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,15 +3,18 @@ import { CreateOrderDTO } from './dtos/create-order.dto';
 import { OrderEntity } from './entities/order.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(OrderEntity)
     private readonly OrderEntity: Repository<OrderEntity>,
+    private readonly paymentService: PaymentService,
   ) {}
 
   async createOrder(createOrderDTO: CreateOrderDTO, cartId: number) {
+    await this.paymentService.createPayment(createOrderDTO);
     return null;
   }
 }
